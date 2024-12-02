c92e80620182f3df8782f41894b160fd76076ac8
Der-Erfeind <jonas.d.sch@web.de>
2024-12-02 15:20:44 +0100

    implemented uart read for network information, untested for now

--- esp32/ws/main/app_main.c
+++ esp32/ws/main/app_main.c
@@ -31,13 +31,18 @@
 #include "lwip/err.h"
 #include "lwip/sys.h"
 
-static const int RX_BUF_SIZE = 1024;
-
-
 static int s_retry_num = 0;
 static EventGroupHandle_t s_wifi_event_group;
 #define WIFI_CONNECTED_BIT BIT0
 #define WIFI_FAIL_BIT      BIT1
+
+
+#define EX_UART_NUM UART_NUM_0
+#define PATTERN_CHR_NUM    (3)         /*!< Set the number of consecutive and identical characters received by receiver which defines a UART pattern*/
+
+#define BUF_SIZE (1024)
+#define RD_BUF_SIZE (BUF_SIZE)
+static QueueHandle_t uart0_queue;
 
 
 #define TXD_PIN (GPIO_NUM_4)
@@ -51,7 +56,9 @@ static const char *uartTAG = "uart";
 static void wifi_event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data);
 static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data);
 static void mqtt_app_start(void);
+static void uart_event_task(void *pvParameters);
 
+char *ssid = "WPA2test", *password = "elephant!", *nettype = "WPA2";
 
 static void log_error_if_nonzero(const char *message, int error_code)
 {
@@ -62,17 +69,30 @@ static void log_error_if_nonzero(const char *message, int error_code)
 
 void init_uart(){
         const uart_config_t uart_config = {
-        .baud_rate = 115200,
+        .baud_rate = 9600,
         .data_bits = UART_DATA_8_BITS,
         .parity = UART_PARITY_DISABLE,
         .stop_bits = UART_STOP_BITS_1,
         .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
         .source_clk = UART_SCLK_DEFAULT,
     };
-    // We won't use a buffer for sending data.
-    uart_driver_install(UART_NUM_1, RX_BUF_SIZE * 2, 0, 0, NULL, 0);
-    uart_param_config(UART_NUM_1, &uart_config);
-    uart_set_pin(UART_NUM_1, TXD_PIN, RXD_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+
+    //Install UART driver, and get the queue.
+    uart_driver_install(EX_UART_NUM, BUF_SIZE * 2, BUF_SIZE * 2, 20, &uart0_queue, 0);
+    uart_param_config(EX_UART_NUM, &uart_config);
+
+    //Set UART log level
+    esp_log_level_set(uartTAG, ESP_LOG_INFO);
+    //Set UART pins (using UART0 default pins ie no changes.)
+    uart_set_pin(EX_UART_NUM, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+
+    //Set uart pattern detect function.
+    uart_enable_pattern_det_baud_intr(EX_UART_NUM, '+', PATTERN_CHR_NUM, 9, 0, 0);
+    //Reset the pattern queue length to record at most 20 pattern positions.
+    uart_pattern_queue_reset(EX_UART_NUM, 20);
+
+    //Create a task to handler UART event from ISR
+    xTaskCreate(uart_event_task, "uart_event_task", 2048, NULL, 12, NULL);
 }
 
 void init_mqtt(){
@@ -169,6 +189,86 @@ void wifi_init(const char *ssid, const char *password, const char *nettype)
 }
 
 
+static void uart_event_task(void *pvParameters)
+{
+    uart_event_t event;
+    size_t buffered_size;
+    uint8_t* dtmp = (uint8_t*) malloc(RD_BUF_SIZE);
+    for (;;) {
+        //Waiting for UART event.
+        if (xQueueReceive(uart0_queue, (void *)&event, (TickType_t)portMAX_DELAY)) {
+            bzero(dtmp, RD_BUF_SIZE);
+            ESP_LOGI(uartTAG, "uart[%d] event:", EX_UART_NUM);
+            switch (event.type) {
+            //Event of UART receving data
+            /*We'd better handler data event fast, there would be much more data events than
+            other types of events. If we take too much time on data event, the queue might
+            be full.*/
+            case UART_DATA:
+                ESP_LOGI(uartTAG, "[UART DATA]: %d", event.size);
+                uart_read_bytes(EX_UART_NUM, dtmp, event.size, portMAX_DELAY);
+                wifi_init(ssid, password, nettype);
+                break;
+            //Event of HW FIFO overflow detected
+            case UART_FIFO_OVF:
+                ESP_LOGI(uartTAG, "hw fifo overflow");
+                // If fifo overflow happened, you should consider adding flow control for your application.
+                // The ISR has already reset the rx FIFO,
+                // As an example, we directly flush the rx buffer here in order to read more data.
+                uart_flush_input(EX_UART_NUM);
+                xQueueReset(uart0_queue);
+                break;
+            //Event of UART ring buffer full
+            case UART_BUFFER_FULL:
+                ESP_LOGI(uartTAG, "ring buffer full");
+                // If buffer full happened, you should consider increasing your buffer size
+                // As an example, we directly flush the rx buffer here in order to read more data.
+                uart_flush_input(EX_UART_NUM);
+                xQueueReset(uart0_queue);
+                break;
+            //Event of UART RX break detected
+            case UART_BREAK:
+                ESP_LOGI(uartTAG, "uart rx break");
+                break;
+            //Event of UART parity check error
+            case UART_PARITY_ERR:
+                ESP_LOGI(uartTAG, "uart parity error");
+                break;
+            //Event of UART frame error
+            case UART_FRAME_ERR:
+                ESP_LOGI(uartTAG, "uart frame error");
+                break;
+            //UART_PATTERN_DET
+            case UART_PATTERN_DET:
+                uart_get_buffered_data_len(EX_UART_NUM, &buffered_size);
+                int pos = uart_pattern_pop_pos(EX_UART_NUM);
+                ESP_LOGI(uartTAG, "[UART PATTERN DETECTED] pos: %d, buffered size: %d", pos, buffered_size);
+                if (pos == -1) {
+                    // There used to be a UART_PATTERN_DET event, but the pattern position queue is full so that it can not
+                    // record the position. We should set a larger queue size.
+                    // As an example, we directly flush the rx buffer here.
+                    uart_flush_input(EX_UART_NUM);
+                } else {
+                    uart_read_bytes(EX_UART_NUM, dtmp, pos, 100 / portTICK_PERIOD_MS);
+                    uint8_t pat[PATTERN_CHR_NUM + 1];
+                    memset(pat, 0, sizeof(pat));
+                    uart_read_bytes(EX_UART_NUM, pat, PATTERN_CHR_NUM, 100 / portTICK_PERIOD_MS);
+                    ESP_LOGI(uartTAG, "read data: %s", dtmp);
+                    ESP_LOGI(uartTAG, "read pat : %s", pat);
+                }
+                break;
+            //Others
+            default:
+                ESP_LOGI(uartTAG, "uart event type: %d", event.type);
+                break;
+            }
+        }
+    }
+    free(dtmp);
+    dtmp = NULL;
+    vTaskDelete(NULL);
+}
+
 static void wifi_event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data)
 {
     if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
@@ -192,16 +292,6 @@ static void wifi_event_handler(void* arg, esp_event_base_t event_base, int32_t e
 }
 
 
-/*
- * @brief Event handler registered to receive MQTT events
- *
- *  This function is called by the MQTT client event loop.
- *
- * @param handler_args user data registered to the event.
- * @param base Event base for the handler(always MQTT Base in this example).
- * @param event_id The id for the received event.
- * @param event_data The data for the event, esp_mqtt_event_handle_t.
- */
 static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
 {
     ESP_LOGD(mqttTAG, "Event dispatched from event loop base=%s, event_id=%" PRIi32, base, event_id);
@@ -262,17 +352,8 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
 
 static void mqtt_app_start(void)
 {
-//    char *ip_address, *ip_address_gateway;
-//    esp_netif_ip_info_t ip_address_t;
-//    esp_netif_get_ip_info(WIFI_IF_STA, &ip_address);
-//    esp_ip4addr_ntoa_r(&ip_address_t.ip, ip_address, sizeof(ip_address));
-//    strncpy(ip_address_gateway, ip_address, sizeof(ip_address)-3);
-//    strcat(ip_address_gateway, "001");
-//    ESP_LOGI(wifiTAG, "IP Adress of Gateway: %s", ip_address_gateway);
-
-
     esp_netif_ip_info_t ip_info;
-    char ip_address_gateway[16], uri_mqtt_broker[32];
+    char ip_address_gateway[16], uri_mqtt_broker[16 + 7];
     esp_netif_t *netif = esp_netif_get_handle_from_ifkey("WIFI_STA_DEF"); 
 
     if (netif == NULL) {
@@ -305,41 +386,13 @@ static void mqtt_app_start(void)
     }
 }
 
-// static void tx_task(void *arg)
-// {
-//     static const char *TX_TASK_uartTAG = "TX_TASK";
-//     esp_log_level_set(TX_TASK_uartTAG, ESP_LOG_INFO);
-//     while (1) {
-//         //sendData(TX_TASK_uartTAG, "Hello world");
-//         vTaskDelay(2000 / portTICK_PERIOD_MS);
-//     }
-// }
-
-//static void rx_task(void *arg)
-//{
-//    static const char *RX_TASK_uartTAG = "RX_TASK";
-//    esp_log_level_set(RX_TASK_uartTAG, ESP_LOG_INFO);
-//    uint8_t* data = (uint8_t*) malloc(RX_BUF_SIZE + 1);
-//    while (1) {
-//        const int rxBytes = uart_read_bytes(UART_NUM_1, data, RX_BUF_SIZE, 1000 / portTICK_PERIOD_MS);
-//        if (rxBytes > 0) {
-//            data[rxBytes] = 0;
-//            ESP_LOGI(RX_TASK_uartTAG, "Read %d bytes: '%s'", rxBytes, data);
-//            ESP_LOG_BUFFER_HEXDUMP(RX_TASK_uartTAG, data, rxBytes, ESP_LOG_INFO);
-//        }
-//    }
-//    free(data);
-//}
-
-
 
 void app_main(void)
 {
-    char *ssid = "WPA2test", *password = "elephant!", *nettype = "WPA2";
 
     ESP_ERROR_CHECK(nvs_flash_init());
     
-    //init_uart();
-    wifi_init(ssid, password, nettype);
+    init_uart();
+//    wifi_init(ssid, password, nettype);
     init_mqtt();
 }
