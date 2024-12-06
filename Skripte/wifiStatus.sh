565c281949ba4cad86d5ea943c8b66b5f74ac8bf
Der-Erzfeind <schmittjo87305@th-nuernberg.de>
2024-12-06 13:08:08 +0100

    added wep password extraction from file to wifiStatus.sh, untested

--- Skripte/wifiStatus.sh
+++ Skripte/wifiStatus.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+read -n 4 connection
+
+if [ "$connection" == "WEP" ]; then
+	ssid=$(cat /etc/NetworkManager/system-connections/Wifi-WEP.nmconnection | sed -n 's/ssid=//p')
+	password=$(cat /etc/NetworkManager/system-connections/Wifi-WEP.nmconnection | sed -n 's/wep-key0=//p')
+	printf "\nSecurity: %s\nSSID: %s\nPassword: %s" $connection $ssid $password
+fi
+	
 sleep 0.1
 printf "\n"
 sudo nmcli dev wifi show-password 
