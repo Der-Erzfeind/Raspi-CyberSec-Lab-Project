484f9d5530b30f1ef2dbf63be313d1f373a8246d
Der-Erzfeind <schmittjo87305@th-nuernberg.de>
2024-12-10 16:48:22 +0100

    added uart disconnect message to wifiReset.sh

--- Skripte/wifiReset.sh
+++ Skripte/wifiReset.sh
@@ -1,2 +1,9 @@
 #! /usr/bin/bash
+
+UART_DEVICE="/dev/ttyS0"  
+UART_SPEED=115200
+stty -F $UART_DEVICE speed $UART_SPEED cs8 -cstopb -parenb
+
+echo "disconnect" > "$UART_DEVICE"
+sleep 1 
 sudo nmcli device down wlan1 && echo "Wifi deactivated"
